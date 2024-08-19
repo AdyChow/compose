b92d72651ba8091666cf2e7c42347922ab23b967
zhouxinyuan <zhouxinyuan@hellogroup.com>
2024-08-19 16:15:13 +0800

    预览函数

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+
+    val composeBom = platform("androidx.compose:compose-bom:2024.06.00")
+    implementation(composeBom)
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    implementation("androidx.compose.ui:ui-tooling-preview")
 }
