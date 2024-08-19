f03cd09614ac8ae0de03ce047e2d15f9ee48137f
zhouxinyuan <zhouxinyuan@hellogroup.com>
2024-08-19 16:12:27 +0800

    定义可组合函数

--- app/src/main/java/com/programmer/compose/MainActivity.kt
+++ app/src/main/java/com/programmer/compose/MainActivity.kt
@@ -4,15 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContent{
-            Text(text = "Hello World")
+        setContent {
+            MessageCard(name = "Android")
         }
+    }
 
+    @Composable
+    fun MessageCard(name: String) {
+        Text(text = "Hello, $name")
     }
 }
