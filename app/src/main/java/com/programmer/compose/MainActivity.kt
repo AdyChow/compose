15d130066e3398c7516fc17af1d2a13ee7844cb8
zhouxinyuan <zhouxinyuan@hellogroup.com>
2024-08-20 15:26:13 +0800

    在展开消息时显示动画效果

--- app/src/main/java/com/programmer/compose/MainActivity.kt
+++ app/src/main/java/com/programmer/compose/MainActivity.kt
@@ -3,8 +3,11 @@ package com.programmer.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -19,6 +22,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -34,7 +41,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    MessageCard(Message("Android", "Compose is awesome!"))
+//                    MessageCard(Message("Android", "Compose is awesome!"))
+                    Conversation(SampleData.conversation)
                 }
             }
 
@@ -57,7 +65,10 @@ class MainActivity : ComponentActivity() {
 
             Spacer(modifier = Modifier.width(8.dp))
 
-            Column {
+            var isExpanded by remember { mutableStateOf(false) }
+            val surfaceColor by animateColorAsState(if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface)
+
+            Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
                 Text(
                     text = msg.name,
                     color = MaterialTheme.colorScheme.secondary,
@@ -65,10 +76,18 @@ class MainActivity : ComponentActivity() {
                 )
                 Spacer(modifier = Modifier.height(8.dp))
 
-                Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+                Surface(
+                    shape = MaterialTheme.shapes.medium,
+                    shadowElevation = 1.dp,
+                    color = surfaceColor,
+                    modifier = Modifier
+                        .animateContentSize()
+                        .padding(1.dp)
+                ) {
                     Text(
                         text = msg.body,
                         modifier = Modifier.padding(4.dp),
+                        maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                         style = MaterialTheme.typography.bodyMedium
                     )
                 }
@@ -94,14 +113,7 @@ class MainActivity : ComponentActivity() {
         ComposeTheme {
             Surface(modifier = Modifier.fillMaxSize()) {
 //                MessageCard(Message("Android", "Compose is awesome!"))
-                val list = arrayListOf(Message("Android", "Compose is awesome!"),
-                    Message("Android2", "Compose is awesome!"),
-                    Message("Android3", "Compose is awesome!dsldkls"),
-                    Message("Android4", "Compose is awesome!dsldkl"),
-                    Message("Android5", "Compose is awesomed\ndjksdjksjk!"),
-                    Message("Android6", "Compose is awesome!\ndjkf\njslf\ndsjk")
-                )
-                Conversation(list)
+                Conversation(SampleData.conversation)
             }
         }
     }
---
+++ app/src/main/java/com/programmer/compose/SampleData.kt
@@ -0,0 +1,12 @@
+package com.programmer.compose
+
+object SampleData {
+    val conversation = arrayListOf(
+        MainActivity.Message("Android", "Compose is awesome!"),
+        MainActivity.Message("Android2", "Compose is awesome!"),
+        MainActivity.Message("Android3", "Compose is awesome!dsldkls"),
+        MainActivity.Message("Android4", "Compose is awesome!dsldkl"),
+        MainActivity.Message("Android5", "Compose is awesomed\ndjksdjksjk!"),
+        MainActivity.Message("Android6", "Compose is awesome!\ndjkf\njslf\ndsjk")
+    )
+}
