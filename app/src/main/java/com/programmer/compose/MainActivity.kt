1611b973ab6046028b23398d1b4b6105af08429f
zhouxinyuan <zhouxinyuan@hellogroup.com>
2024-08-20 14:30:50 +0800

    启用深色主题

--- app/src/main/java/com/programmer/compose/MainActivity.kt
+++ app/src/main/java/com/programmer/compose/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.programmer.compose
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,15 +15,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.programmer.compose2.ui.theme.ComposeTheme
 
 class MainActivity : ComponentActivity() {
 
@@ -30,7 +32,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            MaterialTheme {
+            ComposeTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
                     MessageCard(Message("Android", "Compose is awesome!"))
                 }
@@ -50,7 +52,7 @@ class MainActivity : ComponentActivity() {
                 modifier = Modifier
                     .size(40.dp)
                     .clip(CircleShape)
-                    .border(3.dp, MaterialTheme.colors.primary, CircleShape)
+                    .border(3.dp, MaterialTheme.colorScheme.primary, CircleShape)
             )
 
             Spacer(modifier = Modifier.width(8.dp))
@@ -58,16 +60,16 @@ class MainActivity : ComponentActivity() {
             Column {
                 Text(
                     text = msg.name,
-                    color = MaterialTheme.colors.secondary,
-                    style = MaterialTheme.typography.subtitle1
+                    color = MaterialTheme.colorScheme.secondary,
+                    style = MaterialTheme.typography.titleSmall
                 )
                 Spacer(modifier = Modifier.height(8.dp))
 
-                Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
                     Text(
                         text = msg.body,
                         modifier = Modifier.padding(4.dp),
-                        style = MaterialTheme.typography.body2
+                        style = MaterialTheme.typography.bodyMedium
                     )
                 }
 
@@ -76,10 +78,10 @@ class MainActivity : ComponentActivity() {
 
     }
 
-    @Preview
+    @Preview(uiMode = UI_MODE_NIGHT_YES)
     @Composable
     fun PreviewMessageCard() {
-        MaterialTheme {
+        ComposeTheme {
             Surface(modifier = Modifier.fillMaxSize()) {
                 MessageCard(Message("Android", "Compose is awesome!"))
             }
